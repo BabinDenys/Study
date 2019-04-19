efae9d2f0c4b136b1fa01be5ed0f6c36521e2a47
Denys <denbabinis@gmail.com>
2019-04-19 17:21:07 +0200

    Это наш первый комит

---
+++ project/ToSpeakWith.js
@@ -0,0 +1,10 @@
+alert("Hello World");
+var name = prompt("Как вас зовут?");
+var age = prompt("Сколько вам лет?");
+var happy = confirm("Вы счастливы?");
+var persone = {
+    name: name,
+    age: age,
+    happy: happy
+};
+console.log(persone);
