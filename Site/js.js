003f1f79d50eaf02ba9872f776183bd3397a1d45
Denys <denbabinis@gmail.com>
2019-06-07 21:04:35 +0200

    Start My Site and MyProjects

--- Site/js.js
+++
@@ -1,6 +0,0 @@
-let cirkle = document.getElementsByTagName("circle"),
-    rect = document.querySelector("rect");
-
-    rect.addEventListener( "mouseenter",function(){
-        alert("Вы проиграли!!!");
-    });
